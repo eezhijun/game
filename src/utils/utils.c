c7635f241799d9296e51ad8b575231e3357c53a3
hongzhiijun <hong.zhijun@wuqi-tech.com>
2023-08-17 11:38:09 +0800

    style: copy swap.c to utils.c

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,4 +1,6 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 #include "math.h"
 
 /* function template */
@@ -45,4 +47,46 @@ double rounddown(double num)
     } else {
         return ceil(num);
     }
+}
+
+void swap(void *lhs, void *rhs, size_t size)
+{
+    if (lhs == NULL || rhs == NULL || size == 0) {
+        return;
+    }
+    void *t = malloc(size);
+    memcpy(t, lhs, size);
+    memcpy(lhs, rhs, size);
+    memcpy(rhs, t, size);
+    free(t);
+}
+
+void swap_by_temp(int *lhs, int *rhs)
+{
+    if (lhs == NULL || rhs == NULL) {
+        return;
+    }
+    int temp = *lhs;
+    *lhs = *rhs;
+    *rhs = temp;
+}
+
+void swap_by_sum(int *lhs, int *rhs)
+{
+    if (lhs == NULL || rhs == NULL) {
+        return;
+    }
+    *lhs = *lhs + *rhs;
+    *rhs = *lhs - *rhs;
+    *lhs = *lhs - *rhs;
+}
+
+void swap_by_xor(int *lhs, int *rhs)
+{
+    if (lhs == NULL || rhs == NULL) {
+        return;
+    }
+    *lhs = *lhs ^ *rhs;
+    *rhs = *lhs ^ *rhs;
+    *lhs = *lhs ^ *rhs;
 }
